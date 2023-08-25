9220185dcbc089c8c5fe1109a015932e16fe4673
MICHAELKITH <michaelkithinji27@gmail.com>
2023-08-25 20:50:12 +0300

    Add footer screen

---
+++ screens/FooterScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FooterScreen = () => {
+  return (
+    <View>
+      <Text>FooterScreen</Text>
+    </View>
+  )
+}
+
+export default FooterScreen
